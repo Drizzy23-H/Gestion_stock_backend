5001b62d5f19bd8ee511580de3fcc1b8fa7c7c64
Ben <benjaminkatabanga@gmail.com>
2026-02-05 13:51:28 +0200

    Correction : logique de suppression mise à jour

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { register, login,deleteUser } = require("../controllers/authController");
+const { register, login, deleteUser } = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
 const checkRole = require("../middleware/roleMiddleware");
 
